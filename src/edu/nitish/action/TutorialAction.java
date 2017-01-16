e92c7e31a3883cde800806ddf4f46d76c5c3c4db
nitish <nitishnaidi@gmail.com>
2017-01-16 03:17:28 -0600

    accessing input params and interceptors

--- src/edu/nitish/action/TutorialAction.java
+++ src/edu/nitish/action/TutorialAction.java
@@ -4,10 +4,12 @@ import edu.nitish.service.StarterService;
 
 public class TutorialAction {
 	private String tutorialSiteName;
-
+	private String language;
+	private int age;
+	
 	public String execute() {
 		StarterService starterService = new StarterService();
-		setTutorialSiteName(starterService.getBestTutorialSite());
+		setTutorialSiteName(starterService.getBestTutorialSite(language, age));
 		return "success";
 	}
 
@@ -17,5 +19,21 @@ public class TutorialAction {
 
 	public void setTutorialSiteName(String tutorialSiteName) {
 		this.tutorialSiteName = tutorialSiteName;
+	}
+
+	public String getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
 	}
 }
--- src/edu/nitish/service/StarterService.java
+++ src/edu/nitish/service/StarterService.java
@@ -1,7 +1,7 @@
 package edu.nitish.service;
 
 public class StarterService {
-	public String getBestTutorialSite(){
-		return "www.google.com";
+	public String getBestTutorialSite(String lang, int age){
+		return "www.google.com/"+lang+"/age/"+age;
 	}
 }
